3ed711b9bb3f549e11e1fbdf3fe08ed5899b0db3
Opariin <83518568+Opariin@users.noreply.github.com>
2023-01-07 21:22:14 +0100

    Add files via upload

---
+++ RegisterPagina/registreerIndex.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="registreerStyle.css">
+    <title>Register</title>
+</head>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<body>
+    <div class="container">
+        <header>
+            <h1>Registreer</h1>
+        </header>
+        <div class="container2">
+            <p id="message">
+                
+            </p>
+            <form action="#" method="post" id="manage_user">
+                <fieldset>
+                    <label for="Gebruikersnaam">Gebruikersnaam
+                        <input type="text" name="gebruikersnaam" id="gebruikersnaam">
+                    </label>
+                    <label for="wachtwoord">Wachtwoord
+                        <input type="password" minlength="8" name="wachtwoord" id="wachtwoord">
+                    </label>
+                    <label for="wwherhalen">Wachtwoord Herhalen
+                        <input type="password" name="wwherhalen" id="wwherhalen">
+                    </label>
+                    <label for="email">Email
+                        <input type="email" name="email" id="email">
+                    </label>
+                </fieldset>
+                <button class="btn btn-success" type="submit" id="submit">Submit</button>
+            </form>
+        </div>
+    </div>
+
+    <footer>
+        <div class="fcontainer">
+            <div class="row">
+                <div class="links">
+                    <div class="row">
+                        <div class="wrapper">
+                            <p class="h3-hidden">Footer</p>
+                            <ul id="footer-submenu">
+                                <li><a id="footerlink1" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                                <li><a id="footerlink2" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                                <li><a id="footerlink3" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                                <li><a id="footerlink4" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                            </ul>
+                        </div>
+                        <div class="wrapper">
+                            <p class="h3-hidden">Footer</p>
+                            <ul id="footer-submenu">
+                                <li><a id="footerlink1" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                                <li><a id="footerlink2" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                                <li><a id="footerlink3" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                                <li><a id="footerlink4" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                            </ul>
+                        </div>
+                        <div class="wrapper">
+                            <p class="h3-hidden">Footer</p>
+                            <ul id="footer-submenu">
+                                <li><a id="footerlink1" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                                <li><a id="footerlink2" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                                <li><a id="footerlink3" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                                <li><a id="footerlink4" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                            </ul>
+                        </div>
+                        <div class="wrapper">
+                            <p class="h3-hidden">Footer</p>
+                            <ul id="footer-submenu">
+                                <li><a id="footerlink1" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                                <li><a id="footerlink2" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                                <li><a id="footerlink3" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                                <li><a id="footerlink4" class="costum-page-link" href="#homepage" title>Lorem</a> </li>
+                            </ul>
+                        </div>
+                        <div class="bottomlinks">
+                            <p>© 2022 - Flowerpower.nl - ontwikkeld door Aaron, Berdj, Noah, Junior, Akif</p>
+                        </div>
+                      
+    </footer> 
+
+</body>
+<script src="registreerscript.js"></script>
+</html>
